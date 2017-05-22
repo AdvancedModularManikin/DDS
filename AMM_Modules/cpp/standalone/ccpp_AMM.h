87af545c78a11b4fd18905dd5b242257db2ca45f
Doug Raum <dougr@vcom3d.com>
2017-05-22 12:19:32 -0400

    Added new BG wrapper (to replaced threaded version)

---
+++ AMM_Modules/cpp/standalone/ccpp_AMM.h
@@ -0,0 +1,10 @@
+#ifndef CCPP_AMM_H
+#define CCPP_AMM_H
+
+#include "ccpp.h"
+#include "AMM.h"
+#include "AMMDcps.h"
+#include <orb_abstraction.h>
+#include "AMMDcps_impl.h"
+
+#endif /* CCPP_AMM_H */
