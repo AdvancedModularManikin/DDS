a47e77b2c8c4df80ac934a5259cdcdb5b88e367a
Doug Raum <dougr@vcom3d.com>
2017-07-14 14:42:14 -0400

    Add compound key to Node and HighFrequencyNode

--- AMM_Modules/cpp/src/AMM/PhysiologyEngineManager.cpp
+++ AMM_Modules/cpp/src/AMM/PhysiologyEngineManager.cpp
@@ -163,13 +163,10 @@ void PhysiologyEngineManager::WriteNodeData(string node) {
 void PhysiologyEngineManager::PublishData(bool force = false) {
 	std::map<std::string, double (BioGearsThread::*)()>::iterator it = nodePathMap.begin();
 	while (it != nodePathMap.end()) {
-		WriteNodeData(it->first);
-		/*if ((std::find(bg->highFrequencyNodes.begin(), bg->highFrequencyNodes.end(), it->first) != bg->highFrequencyNodes.end())
+		if ((std::find(bg->highFrequencyNodes.begin(), bg->highFrequencyNodes.end(), it->first) != bg->highFrequencyNodes.end())
 				|| (lastFrame % 10) == 0 || force) {
-			cout << " -Publishing " << it->first << endl;
 			WriteNodeData(it->first);
-		}*/
-
+		}
 		it++;
 	}
 }
