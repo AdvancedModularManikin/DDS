c369001bbdbac14323808da6ce26e5c32df89783
Doug Raum <dougr@vcom3d.com>
2017-09-01 17:52:59 +0000

    Adjustments to Unity Bridge code

--- AMM_Modules/src/UnityBridge.cpp
+++ AMM_Modules/src/UnityBridge.cpp
@@ -1,19 +1,40 @@
 #include "stdafx.h"
 
+#include <map>
 #include <boost/assign/std/vector.hpp>
 #include <boost/assign/list_of.hpp>
-#include <map>
+#include <boost/algorithm/string.hpp>
 
 #include "AMM/DDS_Manager.h"
 #include "AMM/TCPServer.h"
-
 
 using namespace std;
 
 bool closed = false;
 
-std::map<std::string, double> labNodes;
+Publisher *command_publisher;
+Subscriber *command_subscriber;
+Subscriber *node_subscriber;
+TCPServer tcp;
 
+std::vector<std::string> publishNodes = {
+        "EXIT",
+        "SIM_TIME",
+        "Cardiovascular_HeartRate",
+        "Cardiovascular_Arterial_Systolic_Pressure",
+        "Cardiovascular_Arterial_Diastolic_Pressure",
+        "Cardiovascular_Arterial_Mean_Pressure",
+        "Cardiovascular_CentralVenous_Mean_Pressure",
+        "MetabolicPanel_CarbonDioxide",
+        "BloodChemistry_Oxygen_Saturation",
+        "Respiratory_Respiration_Rate",
+        "Energy_Core_Temperature",
+        "Cardiovascular_Arterial_Pressure",
+        "Respiratory_CarbonDioxide_Exhaled",
+        "ECG"
+};
+
+std::map<std::string, double> labNodes;
 void InitializeLabNodes() {
     labNodes["Substance_Sodium"] = 0.0f;
     labNodes["MetabolicPanel_CarbonDioxide"] = 0.0f;
@@ -33,55 +54,53 @@ void InitializeLabNodes() {
 }
 
 
-std::vector<std::string> publishNodes = {
-        "EXIT",
-        "SIM_TIME",
-        "Cardiovascular_HeartRate",
-        "Cardiovascular_Arterial_Systolic_Pressure",
-        "Cardiovascular_Arterial_Diastolic_Pressure",
-        "Cardiovascular_Arterial_Mean_Pressure",
-        "Cardiovascular_CentralVenous_Mean_Pressure",
-        "MetabolicPanel_CarbonDioxide",
-        "BloodChemistry_Oxygen_Saturation",
-        "Respiratory_Respiration_Rate",
-        "Energy_Core_Temperature",
-        "Cardiovascular_Arterial_Pressure",
-        "Respiratory_CarbonDioxide_Exhaled",
-        "ECG"
-};
 
-TCPServer tcp;
 
 void PublishLabs() {
-    std::map<std::string, double>::iterator it = labNodes.begin();
-    while(it != labNodes.end())
+  cout << "Publishing the lab nodes!" << endl;
+  std::map<std::string, double>::iterator it = labNodes.begin();
+  while(it != labNodes.end())
     {
-        std::ostringstream messageOut;
-        messageOut << it->first << "=" << it->second << "|";
-        tcp.Send(messageOut.str());
-        it++;
+      std::ostringstream messageOut;
+      messageOut << it->first << "=" << it->second << "|";
+      tcp.Send(messageOut.str());
+      it++;
     }
-    tcp.Send("Successful connection");
-    tcp.clean();
+  tcp.clean();
 }
 
-void *loop(void *m) {
-    pthread_detach(pthread_self());
-    while (!closed) {
-        string str = tcp.getMessage();
-        if (str != "") {
-            cout << "[CLIENT] Message:" << str << endl;
+void *tcpClientLoop(void *m) {
+  pthread_detach(pthread_self());
+  std::string actionPrefix = "ACT=";
+  std::string requestPrefix = "REQUEST=";
+  
+  while (!closed) {
+    string str = tcp.getMessage();
+    if (str != "") {
+      boost::trim_right(str);
+      cout << "[CLIENT] Message:" << str << endl;
 
-            if (str == "REQUEST=LABS") {
-                PublishLabs();
-            }
-
-            // tcp.Send("Successful connection");
-            tcp.clean();
-        }
-        usleep(1000);
+      // It's a request
+      if(str.substr(0, requestPrefix.size()) == requestPrefix) {
+	std::string request = str.substr(requestPrefix.size());
+	if (request.compare("LABS") == 0) {
+	  PublishLabs();
+	}
+      }
+      
+      // It's an action
+      if(str.substr(0, actionPrefix.size()) == actionPrefix) {
+	std::string action = str.substr(actionPrefix.size());
+	cout << "[CLIENT] Posting action to AMM: " << action << endl;
+	AMM::PatientAction::BioGears::Command cmdInstance;
+	cmdInstance.message(action);
+	command_publisher->write(&cmdInstance);
+      }
+      //      tcp.clean();
     }
-    tcp.detach();
+    usleep(1000);
+  }
+  tcp.detach();
 }
 
 class GenericListener : public ListenerInterface {
@@ -93,7 +112,7 @@ public:
         }
 
         if(labNodes.find(n.nodepath()) != labNodes.end()) {
-            labNodes[n.nodepath()] = n.dbl();
+	  labNodes[n.nodepath()] = n.dbl();
         }
 
         if (std::find(publishNodes.begin(), publishNodes.end(), n.nodepath()) != publishNodes.end()) {
@@ -127,15 +146,15 @@ int main(int argc, char *argv[]) {
     node_sub_listener->SetUpstream(&al);
     command_sub_listener->SetUpstream(&al);
 
-    Subscriber *node_subscriber = mgr->InitializeNodeSubscriber(node_sub_listener);
-    Subscriber *command_subscriber = mgr->InitializeCommandSubscriber(command_sub_listener);
-    Publisher *command_publisher = mgr->InitializeCommandPublisher(pub_listener);
+    node_subscriber = mgr->InitializeNodeSubscriber(node_sub_listener);
+    command_subscriber = mgr->InitializeCommandSubscriber(command_sub_listener);
+    command_publisher = mgr->InitializeCommandPublisher(pub_listener);
 
     cout << "=== [UnityBridge] Ready ..." << endl;
 
     pthread_t msg;
     tcp.setup(9015);
-    if (pthread_create(&msg, NULL, loop, (void *) 0) == 0) {
+    if (pthread_create(&msg, NULL, tcpClientLoop, (void *) 0) == 0) {
         tcp.receive();
     }
 
