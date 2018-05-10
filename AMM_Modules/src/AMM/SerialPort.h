c0443fc348c6481c1805edcce4a2d60c80e8ddf3
Doug Raum <dougr@vcom3d.com>
2018-05-10 17:12:02 -0400

    Rewriting the serial bridge

---
+++ AMM_Modules/src/AMM/SerialPort.h
@@ -0,0 +1,53 @@
+#define BOOST_SIGNALS_NO_DEPRECATION_WARNING true
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/signals.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/array.hpp>
+
+#include <vector>
+#include <deque>
+
+
+class SerialPort
+        : public boost::enable_shared_from_this<SerialPort>
+{
+public:
+    static uint32_t const k_readBufferSize = 1024;
+
+    typedef boost::shared_ptr<std::vector<char> > SharedBufferPtr_t;
+
+    SerialPort(
+            boost::asio::io_service &rIoService,
+            uint32_t baud,
+            std::string const& device);
+
+    ~SerialPort();
+
+    int Initialize();
+    void Close();
+
+    void Write(std::string const& msg);
+
+    boost::signal<void (boost::array<char,k_readBufferSize> const&, size_t bytesTransferred)> DataRead;
+
+private:
+    void BeginRead_();
+    void EndRead_(boost::system::error_code const& error, size_t bytesTransferred);
+
+    void DoWrite_(SharedBufferPtr_t pBuffer);
+    void BeginWrite_();
+    void EndWrite_(boost::system::error_code const& error, size_t bytes_transferred);
+
+    void DoClose_(boost::system::error_code const& error);
+
+    bool m_bInitialized;
+    boost::asio::io_service& m_rIoService;
+    boost::asio::serial_port m_serialPort;
+    boost::array<char,k_readBufferSize> m_rdBuf;
+    std::deque<SharedBufferPtr_t> m_wrBuf;
+
+    uint32_t m_bytesTransferred;
+};
--- AMM_Modules/src/Serial_Bridge.cpp
+++ AMM_Modules/src/Serial_Bridge.cpp
@@ -2,29 +2,53 @@
 
 #include "AMM/DDS_Manager.h"
 
+#include "AMM/SerialPort.h"
+
 #include <boost/algorithm/string.hpp>
 #include <boost/asio.hpp>
-#include <boost/algorithm/string.hpp>
+#include <boost/bind.hpp>
+#include <boost/thread.hpp>
+
+using namespace ::boost::asio;
+using namespace std;
 
 #define PORT_LINUX "/dev/tty96B0"
 // #define PORT_LINUX "/dev/ttyACM0"
 // #define PORT_LINUX "/dev/serial/by-id/usb-Adafruit_Feather_32u4-if00"
 #define PORT_WINDOW "COM7"
 
-
-using namespace ::boost::asio;
-using namespace std;
-
-// Base serial settings
-serial_port_base::baud_rate BAUD(9600);
-serial_port_base::flow_control FLOW(serial_port_base::flow_control::none);
-serial_port_base::parity PARITY(serial_port_base::parity::none);
-serial_port_base::stop_bits STOP(serial_port_base::stop_bits::one);
-
-// Daemonize by default
 int daemonize = 1;
-
 bool closed = false;
+bool transmit = false;
+std::string reportPrefix = "[REPORT]";
+std::string actionPrefix = "[ACT]";
+std::string xmlPrefix = "<?xml";
+std::string msg1 = "[Scenario]m1s1=mule1_scene1";
+std::string msg2 = "[Config_Data]sound_alarm=false";
+Publisher *command_publisher;
+
+void readHandler(boost::array<char,SerialPort::k_readBufferSize> const& buffer, size_t bytesTransferred) {
+    std::string rsp;
+    std::copy(buffer.begin(), buffer.begin()+bytesTransferred, std::back_inserter(rsp));
+
+    if (!rsp.compare(0, reportPrefix.size(), reportPrefix)) {
+        std::string value = rsp.substr(reportPrefix.size());
+        cout << "=== [SERIAL] Making REPORT: " << value << endl;
+    } else if (!rsp.compare(0, actionPrefix.size(), actionPrefix)) {
+        std::string value = rsp.substr(actionPrefix.size());
+        cout << "=== [SERIAL] Sending a command: " << value << endl;
+        AMM::PatientAction::BioGears::Command cmdInstance;
+        boost::trim_right(value);
+        cmdInstance.message(value);
+        command_publisher->write(&cmdInstance);
+    }  else if (!rsp.compare(0, xmlPrefix.size(), xmlPrefix)) {
+        std::string value = rsp;
+        cout << "=== [SERIAL] Recieved an XML snippet: " << value << endl;
+        transmit = true;
+    } else {
+       cout << "=== [SERIAL][DEBUG] " << rsp << endl;
+    }
+}
 
 class GenericSerialListener : public ListenerInterface {
     void onNewNodeData(AMM::Physiology::Node n) {
@@ -44,9 +68,6 @@ static void show_usage(const std::string &name) {
     cerr << "Usage: " << name << " <option(s)>" << "\nOptions:\n" << "\t-h,--help\t\tShow this help message\n" << endl;
 }
 
-
-
-
 int main(int argc, char *argv[]) {
     cout << "=== [AMM - Serial Module Bridge] ===" << endl;
 
@@ -62,21 +83,14 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    io_service io;
-    serial_port port(io, PORT_LINUX);
-
-    port.set_option(BAUD);
-    port.set_option(FLOW);
-    port.set_option(PARITY);
-    port.set_option(STOP);
-
+    // Set up DDS
     const char* nodeName = "AMM_Serial_Bridge";
     auto *mgr = new DDS_Manager(nodeName);
 
     auto * node_sub_listener = new DDS_Listeners::NodeSubListener();
     auto * command_sub_listener = new DDS_Listeners::CommandSubListener();
     auto * pub_listener = new DDS_Listeners::PubListener();
-    
+
     GenericSerialListener al;
     node_sub_listener->SetUpstream(&al);
     command_sub_listener->SetUpstream(&al);
@@ -84,7 +98,22 @@ int main(int argc, char *argv[]) {
     mgr->InitializeSubscriber(AMM::DataTypes::nodeTopic, AMM::DataTypes::getNodeType(), node_sub_listener);
     mgr->InitializeSubscriber(AMM::DataTypes::commandTopic, AMM::DataTypes::getCommandType(), command_sub_listener);
 
-    Publisher *command_publisher = mgr->InitializePublisher(AMM::DataTypes::commandTopic, AMM::DataTypes::getCommandType(), pub_listener);
+    command_publisher = mgr->InitializePublisher(AMM::DataTypes::commandTopic, AMM::DataTypes::getCommandType(), pub_listener);
+
+    // Set up serial
+    io_service io;
+    SerialPort serialPort(io, 115200, PORT_LINUX);
+    serialPort.DataRead.connect(&readHandler);
+    boost::thread t(
+            boost::bind(
+                    &boost::asio::io_service::run,
+                    &io));
+
+    if(serialPort.Initialize())
+    {
+        std::cerr << "Initialization failed!" << std::endl;
+        return 1;
+    }
 
 
     // Publish module configuration once we've set all our publishers and listeners
@@ -102,52 +131,13 @@ int main(int argc, char *argv[]) {
 
     cout << "=== [Serial_Bridge] Ready ..." << endl;
 
-    std::string reportPrefix = "[REPORT]";
-    std::string actionPrefix = "[ACT]";
-    std::string xmlPrefix = "<?xml";
-
     while (!closed) {
-        char c;
-        std::string rsp;
-
-        while (boost::asio::read(port, boost::asio::buffer(&c, 1)) && c != '\n') {
-            rsp += c;
+        if (transmit) {
+            serialPort.Write(msg1);
+            serialPort.Write(msg2);
+            transmit = false;
         }
-
-        if (!rsp.compare(0, reportPrefix.size(), reportPrefix)) {
-            std::string value = rsp.substr(reportPrefix.size());
-            cout << "=== [SERIAL] Making REPORT: " << value << endl;
-        } else if (!rsp.compare(0, actionPrefix.size(), actionPrefix)) {
-            std::string value = rsp.substr(actionPrefix.size());
-            cout << "=== [SERIAL] Sending a command: " << value << endl;
-            AMM::PatientAction::BioGears::Command cmdInstance;
-            boost::trim_right(value);
-            cmdInstance.message(value);
-            command_publisher->write(&cmdInstance);
-        }  else if (!rsp.compare(0, xmlPrefix.size(), xmlPrefix)) {
-            std::string value = rsp;
-            cout << "=== [SERIAL] Recieved an XML snippet: " << value << endl;
-
-
-            std::string msg1 = "[Scenario]m1s1=mule1_scene1";
-            std::string msg2 = "[Config_Data]sound_alarm=false";
-
-            cout << "Sending scenario config.." << endl;
-            boost::asio::write(port, boost::asio::buffer(msg1));
-            cout << "Sending sound_alarm config.." << endl;
-            boost::asio::write(port, boost::asio::buffer(msg2));
-
-        }
-
-        else {
-            cout << "=== [SERIAL][DEBUG] " << rsp << endl;
-        }
-
-
-        if (c != '\n') {
-            cout << "We had a serial timeout of some kind";
-        }
-
+        sleep(1);
     }
 
     cout << "=== [Serial_Bridge] Simulation stopped." << endl;
