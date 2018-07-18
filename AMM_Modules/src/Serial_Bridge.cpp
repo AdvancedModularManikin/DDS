707178ada5ce84f0cd3625bea1b845574643225c
Doug Raum <dougr@vcom3d.com>
2018-07-18 13:30:37 -0600

    Re-add node publishing to serial bridge

--- AMM_Modules/src/Serial_Bridge.cpp
+++ AMM_Modules/src/Serial_Bridge.cpp
@@ -31,42 +31,61 @@ std::string msg4 = "[Capability]detect_button_press=true\n";
 std::string msg5 = "[Config_Data]button_message=some_action_name\n";
 std::string haltingString = "HALTING_ERROR";
 
+
+std::vector <std::string> publishNodes = {
+        "EXIT",
+        "SIM_TIME",
+        "Cardiovascular_HeartRate",
+        "Cardiovascular_Arterial_Systolic_Pressure",
+        "Cardiovascular_Arterial_Diastolic_Pressure",
+        "Cardiovascular_Arterial_Mean_Pressure",
+        "Cardiovascular_CentralVenous_Mean_Pressure",
+        "MetabolicPanel_CarbonDioxide",
+        "BloodChemistry_Oxygen_Saturation",
+        "Respiratory_Respiration_Rate",
+        "Energy_Core_Temperature",
+        "Cardiovascular_Arterial_Pressure",
+        "Respiratory_CarbonDioxide_Exhaled",
+        "ECG",
+        "Respiratory_Tidal_Volume",
+        "Respiratory_LeftPleuralCavity_Volume",
+        "Respiratory_LeftLung_Volume",
+        "Respiratory_RightPleuralCavity_Volume",
+        "Respiratory_RightLung_Volume"
+};
+
 Publisher *command_publisher;
-queue<string> transmitQ;
+queue <string> transmitQ;
 
 // Set up DDS
-const char* nodeName = "AMM_Serial_Bridge";
+const char *nodeName = "AMM_Serial_Bridge";
 DDS_Manager *mgr;
 
-vector<string> explode( const string &delimiter, const string &str)
-{
-  vector<string> arr;
-  
-  int strleng = str.length();
-  int delleng = delimiter.length();
-  if (delleng==0)
-    return arr;//no change
-  
-  int i=0;
-  int k=0;
-  while( i<strleng )
-    {
-      int j=0;
-      while (i+j<strleng && j<delleng && str[i+j]==delimiter[j])
-	j++;
-      if (j==delleng)//found delimiter
-	{
-	  arr.push_back(  str.substr(k, i-k) );
-	  i+=delleng;
-	  k=i;
-	}
-      else
-	{
-	  i++;
-	}
+vector <string> explode(const string &delimiter, const string &str) {
+    vector <string> arr;
+
+    int strleng = str.length();
+    int delleng = delimiter.length();
+    if (delleng == 0)
+        return arr;//no change
+
+    int i = 0;
+    int k = 0;
+    while (i < strleng) {
+        int j = 0;
+        while (i + j < strleng && j < delleng && str[i + j] == delimiter[j])
+            j++;
+        if (j == delleng)//found delimiter
+        {
+            arr.push_back(str.substr(k, i - k));
+            i += delleng;
+            k = i;
+        } else {
+            i++;
+        }
     }
-  arr.push_back(  str.substr(k, i-k) );
-  return arr;
+    arr.push_back(str.substr(k, i - k));
+    return arr;
 }
 
 void sendConfigInfo(std::string scene) {
@@ -76,84 +95,137 @@ void sendConfigInfo(std::string scene) {
     std::string configContent((std::istreambuf_iterator<char>(ifs)),
                               (std::istreambuf_iterator<char>()));
     ifs.close();
-     vector<string> v = explode("\n", configContent);
-    for(int i=0; i<v.size(); i++) {
-    std::this_thread::sleep_for (std::chrono::milliseconds(100));
-      std::string rsp = v[i];
-      transmitQ.push(rsp);
+    vector <string> v = explode("\n", configContent);
+    for (int i = 0; i < v.size(); i++) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::string rsp = v[i];
+        transmitQ.push(rsp);
 
-      }
+    }
 
 }
 
-void readHandler(boost::array<char,SerialPort::k_readBufferSize> const& buffer, size_t bytesTransferred) {
-    std::string inboundBuffer;
-    std::copy(buffer.begin(), buffer.begin()+bytesTransferred, std::back_inserter(inboundBuffer));
+void readHandler(boost::array < char, SerialPort::k_readBufferSize >
+const& buffer,
+size_t bytesTransferred
+) {
+std::string inboundBuffer;
+std::copy(buffer
+.
 
-    vector<string> v = explode("\n", inboundBuffer);
-    for(int i=0; i<v.size(); i++) {
-      std::string rsp = v[i];
-      if (!rsp.compare(0, reportPrefix.size(), reportPrefix)) {
-        std::string value = rsp.substr(reportPrefix.size());
-        cout << "=== [SERIAL] Making REPORT: " << value << endl;
-      } else if (!rsp.compare(0, actionPrefix.size(), actionPrefix)) {
-        std::string value = rsp.substr(actionPrefix.size());
-        cout << "=== [SERIAL] Sending a command: " << value << endl;
-        AMM::PatientAction::BioGears::Command cmdInstance;
-        boost::trim_right(value);
-        cmdInstance.message(value);
-        command_publisher->write(&cmdInstance);
-      }  else if (!rsp.compare(0, xmlPrefix.size(), xmlPrefix)) {
-        std::string value = rsp;
-	
-        cout << "=== [SERIAL] Recieved an XML snippet:" << endl;
-	
-	if (first_message) {
-	  cout << "\t[CAPABILITY_XML] " << value << endl;
-	  first_message = false;
-        mgr->PublishModuleConfiguration(
+begin(), buffer
+
+.
+
+begin()
+
++bytesTransferred,
+std::back_inserter(inboundBuffer)
+);
+
+vector <string> v = explode("\n", inboundBuffer);
+for (
+int i = 0;
+i<v.
+
+size();
+
+i++) {
+std::string rsp = v[i];
+if (!rsp.compare(0, reportPrefix.
+
+size(), reportPrefix
+
+)) {
+std::string value = rsp.substr(reportPrefix.size());
+cout << "=== [SERIAL] Making REPORT: " << value <<
+endl;
+} else if (!rsp.compare(0, actionPrefix.
+
+size(), actionPrefix
+
+)) {
+std::string value = rsp.substr(actionPrefix.size());
+cout << "=== [SERIAL] Sending a command: " << value <<
+endl;
+AMM::PatientAction::BioGears::Command cmdInstance;
+boost::trim_right(value);
+cmdInstance.
+message(value);
+command_publisher->
+write(&cmdInstance);
+}  else if (!rsp.compare(0, xmlPrefix.
+
+size(), xmlPrefix
+
+)) {
+std::string value = rsp;
+
+cout << "=== [SERIAL] Recieved an XML snippet:" <<
+endl;
+
+if (first_message) {
+cout << "\t[CAPABILITY_XML] " << value <<
+endl;
+first_message = false;
+mgr->
+PublishModuleConfiguration(
         mgr
 ->module_id,
-                "liquid_reservoir",
-                "Vcom3D",
-                "liquid_reservoir",
-                "00001",
-                "0.0.1",
-                value
-        );
-	} else {
-	  cout << "\t[STATUS_XML] " << value << endl;
-	  std::size_t found = value.find(haltingString);
-	  if (found!=std::string::npos) {
+"liquid_reservoir",
+"Vcom3D",
+"liquid_reservoir",
+"00001",
+"0.0.1",
+value
+);
+} else {
+cout << "\t[STATUS_XML] " << value <<
+endl;
+std::size_t found = value.find(haltingString);
+if (found!=std::string::npos) {
 mgr->
 SetStatus(mgr
 ->module_id, "liquid_reservoir", HALTING_ERROR);
-	  } else {
+} else {
 mgr->
 SetStatus(mgr
 ->module_id, "liquid_reservoir", OPERATIONAL);
-	  }
-	}
-      } else {
-	if (!rsp.empty() && rsp != "\r") {
-	  cout << "=== [SERIAL][DEBUG] " << rsp << endl;
-	}
-      }
-    }
+}
+}
+} else {
+if (!rsp.
+
+empty() &&
+
+rsp != "\r") {
+cout << "=== [SERIAL][DEBUG] " << rsp <<
+endl;
+}
+}
+}
 }
 
 class GenericSerialListener : public ListenerInterface {
 public:
     void onNewNodeData(AMM::Physiology::Node n) override {
-      if (n.nodepath() == "EXIT") {
-	  cout << "Shutting down simulation based on shutdown node-data from physiology engine."  << endl;
-	  closed = true;
-	  return;
+        if (n.nodepath() == "EXIT") {
+            cout << "Shutting down simulation based on shutdown node-data from physiology engine." << endl;
+            closed = true;
+            return;
+        }
+
+        // Publish values that are supposed to go out on every change
+        if (std::find(publishNodes.begin(), publishNodes.end(), n.nodepath()) != publishNodes.end()) {
+            std::ostringstream messageOut;
+            messageOut << "[AMM_Node_Data]" << n.nodepath() << "=" << n.dbl();
+            string stringOut = messageOut.str();
+            transmitQ.push(messageOut.str());
         }
     }
 
     void onNewCommandData(AMM::PatientAction::BioGears::Command c) override {
-      cout << "We got some command data!  " << c.message() << endl;
+        cout << "We got some command data!  " << c.message() << endl;
         if (!c.message().compare(0, sysPrefix.size(), sysPrefix)) {
             std::string value = c.message().substr(sysPrefix.size());
             if (value.compare("START_SIM") == 0) {
@@ -165,9 +237,9 @@ public:
             } else if (value.compare("RESET_SIM") == 0) {
 
             } else if (!value.compare(0, loadScenarioPrefix.size(), loadScenarioPrefix)) {
-	      
+
                 std::string scene = value.substr(loadScenarioPrefix.size());
-		cout << "Time to load scene " << scene << endl;
+                cout << "Time to load scene " << scene << endl;
                 sendConfigInfo(scene);
             }
         } else {
@@ -199,9 +271,9 @@ int main(int argc, char *argv[]) {
     std::string nodeString(nodeName);
     mgr = new DDS_Manager(nodeName);
 
-    auto * node_sub_listener = new DDS_Listeners::NodeSubListener();
-    auto * command_sub_listener = new DDS_Listeners::CommandSubListener();
-    auto * pub_listener = new DDS_Listeners::PubListener();
+    auto *node_sub_listener = new DDS_Listeners::NodeSubListener();
+    auto *command_sub_listener = new DDS_Listeners::CommandSubListener();
+    auto *pub_listener = new DDS_Listeners::PubListener();
 
     GenericSerialListener al;
     node_sub_listener->SetUpstream(&al);
@@ -210,19 +282,20 @@ int main(int argc, char *argv[]) {
     mgr->InitializeSubscriber(AMM::DataTypes::nodeTopic, AMM::DataTypes::getNodeType(), node_sub_listener);
     mgr->InitializeSubscriber(AMM::DataTypes::commandTopic, AMM::DataTypes::getCommandType(), command_sub_listener);
 
-    command_publisher = mgr->InitializePublisher(AMM::DataTypes::commandTopic, AMM::DataTypes::getCommandType(), pub_listener);
+    command_publisher = mgr->InitializePublisher(AMM::DataTypes::commandTopic, AMM::DataTypes::getCommandType(),
+                                                 pub_listener);
 
     // Set up serial
     io_service io;
     SerialPort serialPort(io, 115200, PORT_LINUX);
     serialPort.DataRead.connect(&readHandler);
-    boost::thread t(
+    boost::thread
+    t(
             boost::bind(
                     &boost::asio::io_service::run,
                     &io));
 
-    if(serialPort.Initialize())
-    {
+    if (serialPort.Initialize()) {
         std::cerr << "Initialization failed!" << std::endl;
         return 1;
     }
@@ -246,13 +319,13 @@ int main(int argc, char *argv[]) {
     cout << "=== [Serial_Bridge] Ready ..." << endl;
 
     while (!closed) {
-        while(!transmitQ.empty()) {
-	  cout << "[SERIAL][SEND_QUEUE] " << transmitQ.front();
-	  serialPort.Write(transmitQ.front());
-	  transmitQ.pop();
+        while (!transmitQ.empty()) {
+            cout << "[SERIAL][SEND_QUEUE] " << transmitQ.front();
+            serialPort.Write(transmitQ.front());
+            transmitQ.pop();
         }
 
-	std::this_thread::sleep_for (std::chrono::milliseconds(250));
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
         cout.flush();
     }
 
