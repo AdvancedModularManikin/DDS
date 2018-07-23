d028d94557eb6fa2f984a0feaf778490dd11c310
Doug Raum <dougr@vcom3d.com>
2018-07-23 11:56:51 -0600

    Remove debug statement

--- AMM_Modules/src/AMM/DDS_Manager.cpp
+++ AMM_Modules/src/AMM/DDS_Manager.cpp
@@ -163,7 +163,6 @@ namespace AMM {
 
     void DDS_Manager::SetStatus(const std::string &local_module_id, const std::string &module_name,
                                 const std::string &capability, AMM::Capability::status_values status) {
-        LOG_TRACE << "Creating instance for " << local_module_id;
         AMM::Capability::Status statusInstance;
         statusInstance.module_id(local_module_id);
         statusInstance.module_name(module_name);
