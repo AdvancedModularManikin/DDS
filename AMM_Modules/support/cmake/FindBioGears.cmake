5503f849a9c141378d9fa3a66971480c78e679d1
Doug Raum <dougr@vcom3d.com>
2018-08-01 17:55:18 -0600

    Updates to cmake for BG 7.0

--- AMM_Modules/support/cmake/FindBioGears.cmake
+++ AMM_Modules/support/cmake/FindBioGears.cmake
@@ -1,68 +1,52 @@
 SET(BIOGEARS_HOME $ENV{BIOGEARS_HOME})
 
 SET(BioGears_INCLUDE_DIRS
-	$ENV{BIOGEARS_HOME}/library/include
-	$ENV{BIOGEARS_HOME}/library/include/bind
-	$ENV{BIOGEARS_HOME}/library/include/cdm
-	$ENV{BIOGEARS_HOME}/library/include/Eigen
-	$ENV{BIOGEARS_HOME}/library/include/schema
-	$ENV{BIOGEARS_HOME}/library/include/log4cpp/include
-)
+        $ENV{BIOGEARS_HOME}/library/include
+        /usr/include/eigen3
+        )
 
 # Find libraries
 FIND_LIBRARY(ENGINE_LIBRARY
-	NAMES
-		BioGearsEngine.so
-	PATHS
-		$ENV{BIOGEARS_HOME}/library/lib/release $ENV{BIOGEARS_HOME}/library/lib
-)
+        NAMES
+        libbiogears.dll libbiogears.a libbiogears.so
+        PATHS
+        $ENV{BIOGEARS_HOME}/library/lib
+        )
 
 FIND_LIBRARY(CDM_LIBRARY
-	NAMES
-		CommonDataModel.so
-	PATHS
-		$ENV{BIOGEARS_HOME}/library/lib/release  $ENV{BIOGEARS_HOME}/library/lib
-)
-
-FIND_LIBRARY(DMB_LIBRARY
-	NAMES
-		DataModelBindings.so
-	PATHS
-		$ENV{BIOGEARS_HOME}/library/lib/release $ENV{BIOGEARS_HOME}/library/lib
-)
+        NAMES
+        libbiogears_cdm.dll libbiogears_cdm.a libbiogears_cdm.so
+        PATHS
+        $ENV{BIOGEARS_HOME}/library/lib
+        )
 
 FIND_LIBRARY(LOG4CPP_LIBRARY
-		NAMES
-		log4cpp
-		PATHS
-		$ENV{BIOGEARS_HOME}/library/lib/release $ENV{BIOGEARS_HOME}/library/lib
-		)
+        NAMES
+        log4cpp
+        )
 
 FIND_LIBRARY(XERCES_LIBRARY
-		NAMES
-		xerces-c
-		PATHS
-		$ENV{BIOGEARS_HOME}/library/lib/release $ENV{BIOGEARS_HOME}/library/lib
-		)
+        NAMES
+        xerces-c
+        )
 
 SET(BioGears_LIBRARIES
-		${XERCES_LIBRARY}
-		${LOG4CPP_LIBRARY}
-		${ENGINE_LIBRARY}
-		${CDM_LIBRARY}
-		${DMB_LIBRARY}
-)
+        ${ENGINE_LIBRARY}
+        ${CDM_LIBRARY}
+        ${XERCES_LIBRARY}
+        ${LOG4CPP_LIBRARY}
+        )
 
 IF (BioGears_INCLUDE_DIRS AND BioGears_LIBRARIES)
-	SET(BioGears_FOUND TRUE)
+    SET(BioGears_FOUND TRUE)
 ENDIF (BioGears_INCLUDE_DIRS AND BioGears_LIBRARIES)
 
 IF (BioGears_FOUND)
-	MESSAGE(STATUS "Found BioGears libraries: ${BioGears_LIBRARIES}")
+    MESSAGE(STATUS "Found BioGears libraries: ${BioGears_LIBRARIES}")
 ELSE (BioGears_FOUND)
-	IF (BioGears_FIND_REQUIRED)
-		MESSAGE(FATAL_ERROR "Could not find BioGears")
-	ENDIF (BioGears_FIND_REQUIRED)
+    IF (BioGears_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could not find BioGears")
+    ENDIF (BioGears_FIND_REQUIRED)
 ENDIF (BioGears_FOUND)
 
 MARK_AS_ADVANCED(BioGears_INCLUDE_DIRS BioGears_LIBRARIES)
