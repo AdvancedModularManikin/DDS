460a69eadfd25acdc1c790efb08e9493a4f58d8f
Doug Raum <dougr@vcom3d.com>
2018-10-27 13:57:50 -0400

    Attempt to make travis build

--- travis_scripts/fastrtps.sh
+++ travis_scripts/fastrtps.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 git clone --recursive https://github.com/eProsima/Fast-RTPS
 cd Fast-RTPS
+git submodule update --init
+git pull
 mkdir build && cd build
 cmake -DTHIRDPARTY=ON -DBUILD_JAVA=ON .. 
 make
