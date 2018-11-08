394cc467688566294c35cf35097d09594c338d65
Doug Raum <dougr@vcom3d.com>
2018-11-08 10:52:25 -0500

    Remove gradle install and java toolset building from Travis CI

--- travis_scripts/fastrtps.sh
+++ travis_scripts/fastrtps.sh
@@ -3,6 +3,6 @@ git clone --recursive https://github.com/eProsima/Fast-RTPS
 cd Fast-RTPS
 git submodule update --init
 mkdir build && cd build
-/usr/local/bin/cmake -DTHIRDPARTY=ON -DBUILD_JAVA=ON .. 
+/usr/local/bin/cmake -DTHIRDPARTY=ON -DBUILD_JAVA=OFF .. 
 make
 sudo make install
