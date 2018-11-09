236030039fe2b6794c9585155c53040473be79a2
Doug Raum <dougr@vcom3d.com>
2018-11-09 12:03:36 -0500

    Add arterial blood pH in addition to venous blood pH

--- AMM_Modules/src/AMM/PhysiologyThread.cpp
+++ AMM_Modules/src/AMM/PhysiologyThread.cpp
@@ -179,6 +179,7 @@ namespace AMM {
             m_pe->GetEngineTrack()->GetDataRequestManager().CreateLiquidCompartmentDataRequest().Set(BGE::VascularCompartment::Aorta, *CO2, "PartialPressure");
             m_pe->GetEngineTrack()->GetDataRequestManager().CreateGasCompartmentDataRequest().Set(BGE::PulmonaryCompartment::Carina, "InFlow");
             m_pe->GetEngineTrack()->GetDataRequestManager().CreatePhysiologyDataRequest().Set("BloodVolume", biogears::VolumeUnit::mL);
+            m_pe->GetEngineTrack()->GetDataRequestManager().CreatePhysiologyDataRequest().Set("ArterialBloodPH");
             m_pe->GetEngineTrack()->GetDataRequestManager().CreatePhysiologyDataRequest().Set("VenousBloodPH");
             m_pe->GetEngineTrack()->GetDataRequestManager().SetResultsFilename(logFilename);
         }
