03f66170f46e3ed01d431b1f6712baa40c0019ea
Doug Raum <dougr@vcom3d.com>
2019-01-30 15:31:23 -0500

    Add KVP parser to utility class

--- AMM_Modules/src/AMM/Utility.hpp
+++ AMM_Modules/src/AMM/Utility.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-#ifndef AMM_UTILITY_H
-#define AMM_UTILITY_H
-
 #include <boost/algorithm/string.hpp>
 #include <boost/archive/iterators/base64_from_binary.hpp>
 #include <boost/archive/iterators/binary_from_base64.hpp>
@@ -77,4 +74,66 @@ std::string getTimestampedFilename(const std::string &basePathname, const std::s
     return filename.str();
 }
 
-#endif
+static std::map<std::string, std::vector<uint8_t>> parse_key_value(std::vector<uint8_t> kv) {
+    std::map<std::string, std::vector<uint8_t>> m;
+    bool keyfound = false;
+    std::string key;
+    std::vector<uint8_t> value;
+    uint8_t prev = '\0';
+
+    if (kv.size() == 0) {
+        goto not_valid;
+    }
+
+    for (uint8_t u8 : kv) {
+        if (keyfound) {
+            if ((u8 == ';') && (prev != ';')) {
+                prev = u8;
+                continue;
+            } else if ((u8 != ';') && (prev == ';')) {
+                if (value.size() == 0) {
+                    goto not_valid;
+                }
+                m[key] = value;
+
+                key.clear();
+                value.clear();
+                keyfound = false;
+            } else {
+                value.push_back(u8);
+            }
+        }
+        if (!keyfound) {
+            if (u8 == '=') {
+                if (key.size() == 0) {
+                    goto not_valid;
+                }
+                keyfound = true;
+            } else if (isalnum(u8)) {
+                key.push_back(u8);
+            } else if ((u8 == '\0') && (key.size() == 0) && (m.size() > 0)) {
+                break; // accept trailing '\0' characters
+            } else if ((prev != ';') || (key.size() > 0)) {
+                goto not_valid;
+            }
+        }
+        prev = u8;
+    }
+    if (keyfound) {
+        if (value.size() == 0) {
+            goto not_valid;
+        }
+        m[key] = value;
+    } else if (key.size() > 0) {
+        goto not_valid;
+    }
+    return m;
+    not_valid:
+    // This is not a failure this is something that can happen because the
+    // participant_qos userData
+    // is used. Other participants in the system not created by rmw could use
+    // userData for something
+    // else.
+    return std::map<std::string, std::vector<uint8_t>>();
+}
+
