0465ab3976a6d81092d0d6f9f819863afa5756c6
Doug Raum <dougr@vcom3d.com>
2019-01-30 15:31:56 -0500

    Create DDS log appender - extends logger to publish DDS diagnostic logging records

---
+++ AMM_Modules/src/AMM/DDS_Log_Appender.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <plog/Log.h>
+#include <plog/Formatters/FuncMessageFormatter.h>
+#include <list>
+#include <AMM/DDS_Manager.h>
+
+using namespace AMM;
+
+namespace plog {
+    template<class Formatter>
+    class DDS_Log_Appender : public IAppender
+    {
+    public:
+        explicit DDS_Log_Appender(DDS_Manager *mgr)
+                : m_mgr(mgr) {
+
+        }
+
+        virtual void write(const Record &record)
+        {
+            util::nstring message = record.getMessage();
+            util::nstring severity = plog::severityToString(record.getSeverity());
+            // util::nstring str = Formatter::format(record);
+            m_mgr->PublishLogRecord(message, severity);
+            //m_mgr->PublishLogRecord(str);
+        }
+
+    private:
+        DDS_Manager *m_mgr;
+    };
+};
+
