2d180d6635a9cfdddaa69350ab1e0194ceb7f005
Doug Raum <dougr@vcom3d.com>
2019-02-10 16:00:26 -0500

    Unlock FastRTPS install from 1.6.0

--- travis_scripts/fastrtps.sh
+++ travis_scripts/fastrtps.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 git clone --recursive https://github.com/eProsima/Fast-RTPS
 cd Fast-RTPS
-git checkout tags/v1.6.0
 git submodule update --init
 mkdir build && cd build
 /usr/local/bin/cmake -DTHIRDPARTY=ON -DBUILD_JAVA=OFF .. 
