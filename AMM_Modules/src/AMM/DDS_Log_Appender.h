05d5d83629f0abf93193db30c36133bb8a751b86
Doug Raum <dougr@vcom3d.com>
2019-02-10 21:55:57 -0500

    Re-enable logging appender for diagnostic logs

--- AMM_Modules/src/AMM/DDS_Log_Appender.h
+++ AMM_Modules/src/AMM/DDS_Log_Appender.h
@@ -9,20 +9,18 @@ using namespace AMM;
 
 namespace plog {
     template<class Formatter>
-    class DDS_Log_Appender : public IAppender
-    {
+    class DDS_Log_Appender : public IAppender {
     public:
         explicit DDS_Log_Appender(DDS_Manager *mgr)
                 : m_mgr(mgr) {
 
         }
 
-        virtual void write(const Record &record)
-        {
+        virtual void write(const Record &record) {
             util::nstring message = record.getMessage();
             util::nstring severity = plog::severityToString(record.getSeverity());
             // util::nstring str = Formatter::format(record);
-                //            m_mgr->PublishLogRecord(message, severity);
+            m_mgr->PublishLogRecord(message, severity);
             //m_mgr->PublishLogRecord(str);
         }
 
