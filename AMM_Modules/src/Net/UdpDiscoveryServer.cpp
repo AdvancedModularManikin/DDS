e162e57f425c4334952ef9806dd85676e1c62fb7
Doug Raum <dougr@vcom3d.com>
2019-05-29 12:40:37 -0400

    Make autodiscovery logging more meaningful

--- AMM_Modules/src/Net/UdpDiscoveryServer.cpp
+++ AMM_Modules/src/Net/UdpDiscoveryServer.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void UdpDiscoveryServer::handle_receive_from(
         const boost::system::error_code &error, size_t bytes_recvd) {
     if (!error && bytes_recvd > 0) {
-        cout << "\tWe got data from " << sender_endpoint_ << ": " << data_ << endl;
+        cout << "\tAutodiscovery request from " << sender_endpoint_ << ": " << data_ << endl;
         // @TODO: Verify this is a valid request before sending data back
         socket_.async_send_to(
                 boost::asio::buffer(data_, bytes_recvd), sender_endpoint_,
@@ -23,7 +23,7 @@ void UdpDiscoveryServer::handle_receive_from(
 
 void UdpDiscoveryServer::handle_send_to(const boost::system::error_code &error,
                                         size_t bytes_sent) {
-    cout << "\tSending data to " << sender_endpoint_ << endl;
+    cout << "\tSending discovery info to " << sender_endpoint_ << endl;
     // @TODO: Send back some sort of validation
     socket_.async_receive_from(
             boost::asio::buffer(data_, max_length), sender_endpoint_,
