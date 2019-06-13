6ef36b5abe9de25dd8730004c569921176a15a92
Doug Raum <draum@wirelessanalytics.com>
2019-06-13 12:28:42 -0400

    Kludge fix for making the plog utils cross-platform

--- AMM_Modules/src/AMM/DDS_Log_Appender.h
+++ AMM_Modules/src/AMM/DDS_Log_Appender.h
@@ -1,39 +1,52 @@
 #pragma once
 
-#include <plog/Log.h>
-#include <plog/Formatters/FuncMessageFormatter.h>
-#include <boost/algorithm/string.hpp>
 #include <AMM/DDS_Manager.h>
+#include <boost/algorithm/string.hpp>
+#include <plog/Formatters/FuncMessageFormatter.h>
+#include <plog/Log.h>
+
+#include <codecvt>
+#include <locale>
+#include <string>
 
 using namespace AMM;
 
-namespace plog {
-    template<class Formatter>
-    class DDS_Log_Appender : public IAppender {
-    public:
-        explicit DDS_Log_Appender(DDS_Manager *mgr)
-                : m_mgr(mgr) {
+namespace plog
+{
+template <class Formatter>
+class DDS_Log_Appender : public IAppender
+{
+  public:
+    explicit DDS_Log_Appender(DDS_Manager* mgr)
+        : m_mgr(mgr)
+    {
+    }
 
+    virtual void write(const Record& record)
+    {
+        std::ostringstream message;
+        const char * cSeverity = plog::severityToString(record.getSeverity());        
+		message << record.getMessage();        
+        std::string severity = std::string(cSeverity);
+        const std::string &smessage = message.str();
+        const std::string &sseverity = severity;
+        
+        if (boost::starts_with(smessage, "Serial debug:"))
+        {
+            return;
         }
 
-        virtual void write(const Record &record) {
-            util::nstring message = record.getMessage();
-            util::nstring severity = plog::severityToString(record.getSeverity());
-            // util::nstring str = Formatter::format(record);
-            if (boost::starts_with(message, "Serial debug:")) {
-                return;
-            }
-
-            try {
-                m_mgr->PublishLogRecord(message, severity);
-            } catch (std::exception &e) {
-                LOG_ERROR << e.what();
-            }
-            //m_mgr->PublishLogRecord(str);
+        try
+        {
+            m_mgr->PublishLogRecord(smessage, sseverity);
         }
+        catch (std::exception& e)
+        {
+            LOG_ERROR << e.what();
+        }
+    }
 
-    private:
-        DDS_Manager *m_mgr;
-    };
+  private:
+    DDS_Manager* m_mgr;
 };
-
+};
